1cc4bb90fb7799a71a961d3a2aad3e086b878f00
Jarek <jarek_m94@wp.pl>
2022-06-18 16:15:23 +0200

    logic in progress

---
+++ app/Card.js
@@ -0,0 +1,18 @@
+class Card {
+    mapTextToSign = {
+        hearts: "&hearts;",
+        spades: "&spades;",
+        diamonds: "&diams;",
+        clubs: "&clubs;"
+    }
+    constructor(weight, type) {
+        this.weight = weight;
+        this.type = type;
+    }
+
+    render() {
+        const card = document.createElement('div');
+        card.setAttribute('class', `card ${this.type}`);
+        card.innerHTML = `${this.weight} ${this.mapTextToSign[this.type]}`;
+    }
+}
